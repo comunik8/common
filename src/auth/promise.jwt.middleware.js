6f8be4a950e49e6736f645788fe31088d49ce06a
Michal Tesar <tesarm@gmail.com>
2018-05-23 11:10:25 +0100

    promise jwt middleware fixed

--- src/auth/promise.jwt.middleware.js
+++ src/auth/promise.jwt.middleware.js
@@ -1,13 +1,9 @@
 import Service from '../service/service';
 import {UnauthorizedHttpError} from '../errors/http.error';
 
-const authenticate = (role = 'user', section = null) => {
-  return (jwt) => {
-
-    if (!jwt) return next(new UnauthorizedHttpError('Only JWT Auth is supported'));
-
-    return Service.User.request('auth.can.access', {jwt, role, section});
-  };
+const authenticate = (jwt, role = 'user', section = null) => {
+  if (!jwt) return next(new UnauthorizedHttpError('Only JWT Auth is supported'));
+  return Service.User.request('auth.can.access', {jwt, role, section});
 };
 
 export default authenticate;
